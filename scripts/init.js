7e70183e90a9faad0b6c476dd6a21bd8e876771f
SEG91 <50146121+SEG91@users.noreply.github.com>
2023-03-21 15:28:20 +0100

    update 1.0.5

---
+++ scripts/init.js
@@ -0,0 +1,41 @@
+Hooks.on("setup", () => {
+	console.log("init:Hooksonsetup");
+CONFIG.DND5E.spellSchools.AncienBlood = "Manipulation du Sang Ancien";
+CONFIG.DND5E.spellSchools.PuissOurs = "Puissance de l'Ours";
+CONFIG.DND5E.spellSchools.ComNat = "Communion avec la Nature";
+CONFIG.DND5E.spellSchools.Ombre = "Domaine de l'Ombre";
+CONFIG.DND5E.spellSchools.Corrupt = "Domaine de la Corruption";
+});
+
+/*Hooks.on('createActor', async function (actor, options, userID) {
+  //check current user is the one that triggered the création
+  //(we wouldn't want to add abilities to the actor multiple times)
+  console.log("init:Hooksoncreateactor");
+  if (userID != game.user.id) { return;}
+  console.log(actor.system.resources.primary.label);
+  actor.system.resources.primary.label="Sang Ancien";
+  console.log(actor.system.resources.primary.label);
+});
+
+function DebugDisplay(app, html, data) {
+	console.log("DebugDisplay>>");
+	console.log(data.system.resources.primary.label);
+	console.log("<<DebugDisplay");
+};
+
+Hooks.on("renderActorSheet", DebugDisplay);*/
+
+function UpdateActorResourceName(actor, data, options, userId) {
+//do my things here
+console.trace();
+actor.system.resources.primary.label="Sang Ancien";
+actor.system.resources.primary.max=(actor.system.abilities.int.value -1)*actor.details.level;
+};
+
+Hooks.on('updateActor', UpdateActorResourceName);
+
+//function declaration (same context) the argument are declared in the same order as the original call
+
+
+
+
