5af7b28c37a7880c43b4c30c0c1066e70d5016e2
SEG91 <50146121+SEG91@users.noreply.github.com>
2023-03-22 21:18:43 +0100

    update 1.0.6

--- scripts/init.js
+++ scripts/init.js
@@ -5,6 +5,8 @@ CONFIG.DND5E.spellSchools.PuissOurs = "Puissance de l'Ours";
 CONFIG.DND5E.spellSchools.ComNat = "Communion avec la Nature";
 CONFIG.DND5E.spellSchools.Ombre = "Domaine de l'Ombre";
 CONFIG.DND5E.spellSchools.Corrupt = "Domaine de la Corruption";
+CONFIG.DND5E.spellSchools.Meute = "Pouvoir de la Meute";
+CONFIG.DND5E.spellSchools.Alpha = "Domination de l'Alpha";
 });
 
 /*Hooks.on('createActor', async function (actor, options, userID) {
